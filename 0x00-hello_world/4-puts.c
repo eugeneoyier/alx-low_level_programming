93f6976cdc08bdc5090d31818a6d35da740fab16
eugeneoyier <eugeneoyier@outlook.com>
2022-06-30 10:59:45 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- *  * main - prints the phrase "Programming is like building a multilingual puzzle
- *   * Return: 0 if exited properly, non-zero otherwise
- *    */
+ * main - program entry point
+ *
+ * Return: 0 always (exit main function)
+ */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
