456dd457d5a8ced430cc6f210e7d4cd269ab708d
eugeneoyier <eugeneoyier@outlook.com>
2022-07-01 19:09:30 -0700

    done

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	int c=0;
+	int c = 0;
 	int f_d;
 	int l_d;
 
@@ -16,17 +16,17 @@ int main(void)
 	int f_d2;
 	int l_d2;
 
-	while (c<=98)
+	while (c <= 98)
 	{
-		f_d = (c/10+'0');
-		l_d = (c%10+'0');
-		c2=0;
-		while (c2<=99)
+		f_d = (c / 10 + '0');
+		l_d = (c % 10 + '0');
+		c2 = 0;
+		while (c2 <= 99)
 		{
-			f_d2 = (c2/10+'0');
-			l_d2 = (c2%10+'0');
+			f_d2 = (c2 / 10 + '0');
+			l_d2 = (c2 % 10 + '0');
 
-			if (c<c2)
+			if (c < c2)
 			{
 				putchar(f_d);
 				putchar(l_d);
@@ -34,7 +34,7 @@ int main(void)
 				putchar(f_d2);
 				putchar(l_d2);
 
-				if (c !=98)
+				if (c != 98)
 				{
 					putchar(',');
 					putchar(32);
