314bf92c349190e1bfe8559c1f65868a41fd22ea
eugeneoyier <eugeneoyier@outlook.com>
2022-07-05 13:27:43 -0700

    isipofanya sijui

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
-/*
-* print_alphabet_x10 -> prints the lowercase alphabet
-*/
+/**
+ * print_alphabet_x10 -> prints the lowercase alphabets
+ */
 
 void print_alphabet_x10(void)
 {
 	int j;
 	int x;
 
-	for (x = 0; x < 10; x++)
+	for(x = 0; x < 10; x++)
 	{
-		for (j = 'a'; j <= 'z'; j++)
+		for (j = 'a'; j<= 'z'; j++)
 		{
 			_putchar(j);
 		}
-		_putchar('\n');
+		_putchar('\n')
 	}
 }
